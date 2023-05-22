013c9ca5d51ce53f8000be5185ffad0ab6397a23
Grant Emerson <grant.alex.emerson@gmail.com>
2023-05-21 20:49:41 -0500

    local cal working

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,40 @@
-import { greetUser } from '$utils/greet';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  const calendarElement = document.querySelector<HTMLElement>('[data-element="calendar"]');
+  if (!calendarElement) return;
+
+  const events = getEvents();
+  console.log({ events });
+
+  const calendar = new Calendar(calendarElement, {
+    plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,listWeek',
+    },
+    events,
+  });
+  calendar.render();
 });
+
+const getEvents = (): Event[] => {
+  const scripts = document.querySelectorAll<HTMLScriptElement>('[data-element="event-data"]');
+  const events = [...scripts].map((script) => {
+    if (!script.textContent) {
+      return;
+    }
+    const event: Event = JSON.parse(script.textContent || '{}');
+    event.start = new Date(event.start);
+    event.end = new Date(event.end);
+    return event;
+  });
+
+  return events;
+};
